f6da259b8af826727b1abcf0bcbd6d61841b5b28
NikTishkov <nik.tishkov25@gmail.com>
2025-05-14 21:56:01 +0700

    HW 6.2 is correct.

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,58 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static boolean counterAge( int year) {
-        if (year % 4 != 0) {
-            return false;
-        } else if (year % 100 != 0) {
-            return true;
+    public static void findALeapYear(int year) {
+        int fourthYear = year % 4;
+        int hundredthYear = year % 100;
+        int fourHundredthYear = year % 400;
+        if (year > 1584 && fourthYear == 0 && hundredthYear != 0) {
+            System.out.println(year + " год — високосный год");
+        } else if (year > 1584 && fourthYear == 0 && fourHundredthYear == 0) {
+            System.out.println(year + " год — високосный год");
+        } else if (year > 1584 && hundredthYear != 0) {
+            System.out.println(year + " год — невисокосный год");
+        } else if (year < 1584) {
+            System.out.println(year + " год — невисокосный год");
+        }
+    }
+
+    public static void checkVersion(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0 && clientDeviceYear >= currentYear) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке.");
+        }
+        if (clientOS == 0 && clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= currentYear) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке.");
+        }
+        if (clientOS == 1 && clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static int findDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            return 1;
+        } else if (deliveryDistance < 60) {
+            return 2;
+        } else if (deliveryDistance <= 100) {
+            return 3;
         } else {
-            return year % 400 == 0;
+            return 0;
         }
-    }
-
-         public static void checkVersion(int clientOS , int clientDeviceYear) {
-             if (clientOS == 0 && clientDeviceYear >= 2015) {
-                 System.out.println("Установите обычную версию приложения для iOS по ссылке.");
-             } else if (clientOS == 0 && clientDeviceYear < 2015) {
-                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-             }
-             if (clientOS == 1 && clientDeviceYear >= 2015) {
-                 System.out.println("Установите обычную версию приложения для Android по ссылке.");
-             } else if (clientOS == 1 && clientDeviceYear < 2015) {
-                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
-             }
-    }
-
-    public static int deliveryDistance(int deliveryDistance) {
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: 1.");
-        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней: 2.");
-        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: 3.");
-        } else if (deliveryDistance > 100) {
-            System.out.println("Доставки нет.");
-        }
-        return deliveryDistance;
     }
 
     public static void main(String[] args) {
         System.out.println("Task 1");
         //Task 1
-        int year = 2021;
-        if (counterAge(year)) {
-            System.out.println(year + " год — високосный год");
-        } else {
-            System.out.println(year +" год — невисокосный год");
-        }
+        findALeapYear(2021);
         System.out.println("Task 2");
         //Task 2
-        int clientOS = 1;
-        int clientDeviceYear = 2015;
-        checkVersion(clientOS,clientDeviceYear);
+        checkVersion(1, 2015);
         System.out.println("Task 3");
         //Task 3
-        int deliveryDistance = 95;
-        deliveryDistance(deliveryDistance);
+        System.out.println("Потребуется дней: " + findDeliveryDays(95));
     }
 }
